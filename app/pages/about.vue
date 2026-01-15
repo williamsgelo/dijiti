4b77655bbed18410e8d99dbc0306b4cfa4c731d2
Angelo Williams <angelowilliams1015@gmail.com>
2026-01-15 11:40:32 +0200

    test no padding

--- app/pages/about.vue
+++ app/pages/about.vue
@@ -15,7 +15,7 @@
 
       <!-- Content Overlay -->
       <div
-        class="relative container mx-auto px-6 lg:px-20 h-full flex items-center [@media(max-height:650px)]:items-start [@media(max-height:650px)]:pt-10"
+        class="relative container mx-auto px-6 lg:px-20 h-full flex items-center [@media(max-height:650px)]:items-start"
       >
         <div class="max-w-3xl">
           <h1
